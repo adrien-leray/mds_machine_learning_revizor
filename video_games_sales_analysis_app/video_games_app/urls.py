883493ec8cad040cadcb5e0eaa17a8b0d3c8f47d
Esteban Le Calvez <lecalvez.esteban@gmail.com>
2020-03-23 14:53:34 +0100

    Ajout d'un pie chart bidon pour tester charts.js

--- video_games_sales_analysis_app/video_games_app/urls.py
+++ video_games_sales_analysis_app/video_games_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('pie-chart/', views.pie_chart, name='pie-chart'),
 ]
--- video_games_sales_analysis_app/video_games_app/views.py
+++ video_games_sales_analysis_app/video_games_app/views.py
@@ -7,6 +7,24 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {'latest_question_list': "null"}
+    return HttpResponse(render(request,"video_games_app/index.html",{}))
 
-    return HttpResponse(render(request,"video_games_app/index.html",context))
+def pie_chart(request):
+    labels = []
+    data = []
+    labels.append("yo1")
+    labels.append("yo2")
+    labels.append("yo3")
+    labels.append("yo4")
+    labels.append("yo5")
+
+    data.append("15")
+    data.append("15")
+    data.append("26")
+    data.append("35")
+    data.append("5")
+
+    return HttpResponse(render(request, 'video_games_app/pie_chart.html', {
+            'labels': labels,
+            'data': data,
+    }))
